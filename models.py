01a58d8381a8442c2544533fde3957bd00d25575
artem <93213014+ArtemBagin@users.noreply.github.com>
2021-10-26 23:11:53 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,11 +4,11 @@ class ShortUrls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     OriginalUrl = db.Column(db.String(300))
     ShortUrl = db.Column(db.String(120), unique=True)
-    ShotCode = db.Column(db.String(120), unique=True)
+    ShortCode = db.Column(db.String(120), unique=True)
 
     def __init__(self,OriginalUrl,ShotUrl,ShotCode):
         self.OriginalUrl = OriginalUrl
-        self.ShotUrl = ShotUrl
-        self.ShotCode = ShotCode
+        self.ShortUrl = ShortUrl
+        self.ShortCode = ShortCode
 
 
